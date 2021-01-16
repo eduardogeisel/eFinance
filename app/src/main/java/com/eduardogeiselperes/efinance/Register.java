07e56e3ea8b0b1d9580d1a92033704a42f8a38f1
Eduardo Peres <eduardogeisel@hotmail.com>
2021-01-15 18:45:26 -0800

    Updated register activity

--- app/src/main/java/com/eduardogeiselperes/efinance/Register.java
+++ app/src/main/java/com/eduardogeiselperes/efinance/Register.java
@@ -1,14 +1,88 @@
 package com.eduardogeiselperes.efinance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText registerEmail;
+    private EditText registerPass;
+    private Button registerBtn;
+    private static final String TAG = "FIREBASE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Initializing firebase instance
+        auth = FirebaseAuth.getInstance();
+
+        //Getting the Id's
+        registerEmail = findViewById(R.id.editTextRegisterEmail);
+        registerPass = findViewById(R.id.editTextTextPassword);
+        registerBtn = findViewById(R.id.btnRegister);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
     }
+
+    private void register() {
+        String email = registerEmail.getText().toString().trim();
+        String password = registerPass.getText().toString().trim();
+
+//        if(!checkEmail()) {
+//            return;
+//        }
+//        if(!checkPassword()) {
+//            return;
+//        }
+//        if(!checkPasswordConfirm()) {
+//            return;
+//        }
+
+//        registerInputLayoutEmail.setErrorEnabled(false);
+//        registerInputLayoutPass.setErrorEnabled(false);
+//        registerInputLayoutPassConfirm.setErrorEnabled(false);
+//
+//        progressBar.setVisibility(View.VISIBLE);
+
+        //create a user
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "user creation: Complete");
+
+                        //progressBar.setVisibility(View.GONE);
+
+                        if(!task.isSuccessful()) {
+                            Log.d(TAG, "Creation failure." + task.getException());
+                        } else {
+                            startActivity(new Intent(Register.this, Login.class));
+                            finish();
+                        }
+                    }
+                });
+        Toast.makeText(getApplicationContext(), "Thank you for registering!", Toast.LENGTH_SHORT).show();
+    }
+
 }
