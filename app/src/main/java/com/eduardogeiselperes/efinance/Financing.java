259d61f9998401b414cf4f8ab35316fd6df8cfaa
Eduardo Peres <eduardogeisel@hotmail.com>
2021-01-18 00:06:07 -0800

    Added home button and updated strings

--- app/src/main/java/com/eduardogeiselperes/efinance/Financing.java
+++ app/src/main/java/com/eduardogeiselperes/efinance/Financing.java
@@ -2,13 +2,27 @@ package com.eduardogeiselperes.efinance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Financing extends AppCompatActivity {
+
+    private Button btnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_financing);
+
+        btnHome = findViewById(R.id.btnHome);
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Financing.this, Welcome.class));
+            }
+        });
     }
 }
