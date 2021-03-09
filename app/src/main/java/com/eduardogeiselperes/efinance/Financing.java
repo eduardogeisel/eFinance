e69355238167a396c1d061de4a8067e8962975f8
Eduardo Peres <eduardogeisel@hotmail.com>
2021-03-09 12:16:12 -0800

    Added a few variables to the Financing.java class

--- app/src/main/java/com/eduardogeiselperes/efinance/Financing.java
+++ app/src/main/java/com/eduardogeiselperes/efinance/Financing.java
@@ -9,16 +9,22 @@ public class Financing {
     public String type;
     public double value;
     public double years;
+    public double downPayment;
+    public double taxes;
+    public double interestRate;
 
     public Financing(){
         //Default constructor to call DataSnapshot.getValue(Financing.class)
     }
 
-    public Financing(String name, String type, double value,  double years) {
+    public Financing(String name, String type, double value, double years, double downPayment, double taxes, double interestRate) {
         this.name = name;
         this.type = type;
         this.value = value;
         this.years = years;
+        this.downPayment = downPayment;
+        this.taxes = taxes;
+        this.interestRate = interestRate;
     }
 
     public String getName() {
@@ -45,12 +51,35 @@ public class Financing {
         this.value = value;
     }
 
-
     public double getYears() {
         return years;
     }
 
     public void setYears(double years) {
         this.years = years;
+    }
+
+    public double getDownPayment() {
+        return downPayment;
+    }
+
+    public void setDownPayment(double downPayment) {
+        this.downPayment = downPayment;
+    }
+
+    public double getTaxes() {
+        return taxes;
+    }
+
+    public void setTaxes(double taxes) {
+        this.taxes = taxes;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
     }
 }
